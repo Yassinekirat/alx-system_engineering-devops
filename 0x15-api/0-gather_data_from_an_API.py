38756cd100639d6ee96651d6dbdc190338b4fdac
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-22 16:52:00 +0100

    pushing all files empty + working on taks 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that uses a REST API to returns information
+about his/her TODO list progress
+"""
+import json
+import sys
+import urllib
+import urllib.request
+import requests
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = base_url + "/" + employee_id
+
+    response = requests.get(url)
+    employee_name = response.json().get('name')
+
+    todo_url = url + "/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
+    completed_tasks_count = 0
+    completed_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            completed_tasks_count += 1
+
+    total_tasks_count = len(tasks)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks_count, total_tasks_count))
+
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
