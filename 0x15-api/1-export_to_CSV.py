83ea5846c8317a5aab8922315d26f1a819b2a06e
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-22 17:03:22 +0100

    task 1 this is hard i needed some help

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+script that export data in the CSV format.
+"""
+import json
+import requests
+import sys
+import urllib
+import urllib.request
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = base_url + "/" + employee_id
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employee_id), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employee_id, username, task.get('completed'),
+                               task.get('title')))
