187653221d19bd4cdf1cfd91149bb02ca7f0f456
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-06-04 12:42:28 +0100

    just checking

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""How many subs are in a given subreddit using the Reddit api"""
+"""
+How many subs are in a given subreddit using the Reddit API.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns How many subs"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return (0)
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    user_agent = {'User-Agent': 'Google Chrome Version 125.0.6422.114'}
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
 
     if response.status_code >= 300:
-        return 0
+        return (0)
+    if 'data' in response.json():
+        return (response.json().get('data').get('subscribers'))
 
-    data = response.json().get("data", {})
-    return data.get("subscribers", 0)
+    else:
+        return (0)
