c27476f7c8d3604e71514b53c650aae3d1bd1bc6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-06-04 12:17:41 +0100

    getting back after 1 month of deferring first task of the day

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns How many subs"""
+    if subreddit is None or not type(subreddit) is str:
+        return (0)
+    
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    user_agent = {'User-Agent': 'Google Chrome Version 125.0.6422.114'}
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code >= 300:
+        return (0)
+    if 'data' in response.json():
+        return (response.json().get('data').get('subscribers'))
+
+    else:
+        return (0)
