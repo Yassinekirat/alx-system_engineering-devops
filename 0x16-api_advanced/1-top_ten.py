1f46adcded73b6dcd83302dd3555cf7f51e2c123
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-06-04 13:03:00 +0100

    idk whats those 2 errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
+prints the titles of he first 10 hot posts
+listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for
-    a given subreddit
-    """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """returns the first 10 hot posts"""
 
-    if response.status_code != 200:
-        print(None)
-        return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Google Chrome Version 125.0.6422.114'}
+    response = requests.get(url, headers=user_agent, params={'limit': 10})
+
+    if 'data' in response.json():
+        for posts in response.json().get('data').get('children'):
             print(posts.get('data').get('title'))
 
     else:
         print(None)
-        
